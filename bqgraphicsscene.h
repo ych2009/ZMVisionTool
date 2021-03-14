02353afc23dce71d556349018732405cbfb17d7a
yoyoych <1079804535@qq.com>
2021-03-13 18:27:20 -0800

    1

---
+++ bqgraphicsscene.h
@@ -0,0 +1,30 @@
+﻿#ifndef BQGRAPHICSSCENE_H
+#define BQGRAPHICSSCENE_H
+
+#include <QGraphicsScene>
+#include "bpointitem.h"
+
+class BQGraphicsScene : public QGraphicsScene
+{
+    Q_OBJECT
+
+public:
+    BQGraphicsScene(QObject *parent = nullptr);
+
+    void startCreate();
+    void startCreateArrowLine();
+
+protected:
+    virtual void mousePressEvent(QGraphicsSceneMouseEvent *event);
+
+signals:
+    void updatePoint(QPointF p, QList<QPointF> list, bool isCenter);
+    void createFinished();
+
+protected:
+    QList<QPointF> m_list;
+    bool is_creating_BPolygon;
+    bool is_creating_ArrowLine;
+};
+
+#endif // BQGRAPHICSSCENE_H
